e2f3fe4977ae1da12a6225470ff5042673d74cfc
Edson Luiz <177874526+EdsonLuiz-Dev@users.noreply.github.com>
2026-08-20 13:40:38 -0300

    [CHORE]: update colored-cards padding, background, and link typography
    Add white background, increase card gap, remove rounded-sm, switch links
    from primary-nav to button-secondary typography.

--- resources/views/blocks/colored-cards.blade.php
+++ resources/views/blocks/colored-cards.blade.php
@@ -8,7 +8,7 @@
   ];
 @endphp
 
-<section class="colored-cards-block py-14 lg:py-24">
+<section class="colored-cards-block bg-white py-14 lg:py-28">
   <div class="container">
 
     {{-- Header row: heading + description --}}
@@ -25,12 +25,12 @@
 
     {{-- Cards grid --}}
     @if (!empty($cards))
-      <div class="grid grid-cols-1 gap-6 lg:grid-cols-3">
+      <div class="grid grid-cols-1 gap-14 lg:grid-cols-3">
         @foreach ($cards as $card)
           @php
             $bgClass = $colorMap[$card['backgroundColor'] ?? 'blue'] ?? 'bg-blue';
           @endphp
-          <div class="{{ $bgClass }} flex flex-col justify-between rounded-sm p-8 lg:p-10">
+          <div class="{{ $bgClass }} flex flex-col justify-between p-8 lg:p-10">
             <div>
               @if (!empty($card['title']))
                 <h3 class="heading-3 text-dark-blue mb-6">{!! wp_kses_post($card['title']) !!}</h3>
@@ -48,13 +48,13 @@
             <div class="mt-auto flex flex-col gap-3">
               @if (!empty($card['link1Text']) && !empty($card['link1Url']))
                 <a href="{{ esc_url($card['link1Url']) }}"
-                  class="primary-nav text-dark-blue decoration-dark-blue underline decoration-2 underline-offset-8">
+                  class="button-secondary text-dark-blue decoration-dark-blue decoration-2 underline-offset-8">
                   {!! wp_kses_post($card['link1Text']) !!}
                 </a>
               @endif
               @if (!empty($card['link2Text']) && !empty($card['link2Url']))
                 <a href="{{ esc_url($card['link2Url']) }}"
-                  class="primary-nav text-dark-blue decoration-dark-blue underline decoration-2 underline-offset-8">
+                  class="button-secondary text-dark-blue decoration-dark-blue decoration-2 underline-offset-8">
                   {!! wp_kses_post($card['link2Text']) !!}
                 </a>
               @endif
