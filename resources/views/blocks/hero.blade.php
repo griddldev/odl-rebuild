21f231b63eb5309ff0841c63da8af60fa760659e
Edson Luiz <177874526+EdsonLuiz-Dev@users.noreply.github.com>
2026-08-20 16:16:41 -0300

    [FIX]: adjust hero block height and controls positioning for mobile

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -1,5 +1,5 @@
 <section class="hero-block relative flex flex-col" data-playing="true" data-muted="true">
-  <div class="relative h-[75vh] min-h-[500px] overflow-hidden">
+  <div class="relative h-full min-h-[950px] overflow-hidden md:h-[75vh] md:min-h-0">
 
     {{-- Background video --}}
     @if (!empty($videoUrl))
@@ -25,7 +25,7 @@
 
     {{-- Video controls --}}
     @if (!empty($videoUrl))
-      <div class="bg-off-white absolute right-0 top-1/2 flex -translate-y-1/2 flex-col items-center gap-3">
+      <div class="bg-off-white absolute right-0 top-[75%] flex -translate-y-1/2 flex-col items-center gap-3 md:top-1/2">
         <button class="hero-play-btn bg-off-white flex items-center justify-center p-6" aria-label="Pause video">
           <img class="hero-icon-pause"
             src="{{ Illuminate\Support\Facades\Vite::asset('resources/images/icons/pause-32.svg') }}" alt=""
@@ -65,7 +65,8 @@
   @endphp
 
   @if (count($links) > 0)
-    <div class="bg-dark-blue absolute bottom-0 z-10 w-[900px] max-w-[900px] translate-y-[50%] self-center">
+    <div
+      class="bg-dark-blue absolute bottom-0 z-10 hidden w-[900px] max-w-[900px] translate-y-[50%] self-center lg:flex">
       <div class="container flex flex-wrap justify-center gap-x-16 gap-y-4 py-5">
         @foreach ($links as $link)
           <x-button :href="$link['url']" :target="$link['target']" class="justify-center">
